5c14cfb8eb6d957252102515364cbbaea16d6972
danielhaendel <daniel.haendel@students.tbs1.de>
2025-11-12 22:36:07 +0100

    Refactor project structure: move `weather_analytics` package to `src` directory and update imports accordingly.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 """WSGI entrypoint for Weather Analytics."""
 
-from weather_analytics import create_app
+from src import create_app
 
 app = create_app()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
